afd80e87cfb81cc19418d7b6ac8956ba89942a49
Puneet Ahuja <pahuja@tower-research.com>
2018-06-13 16:56:31 +0530

    small commit

--- DataPublisher.h
+++ DataPublisher.h
@@ -11,55 +11,52 @@ class DataPublisher
   public:
 	int init()
 	{
-		/* Open windows connection */
-		if (WSAStartup(0x0101, &w) != 0)
+		/* Load Winsock 2.0 DLL */
+		if (WSAStartup(MAKEWORD(2, 0), &m_wsaData) != 0)
 		{
-			fprintf(stderr, "Could not open Windows connection.\n");
-			exit(0);
-		}
-
-		/* Open a datagram socket */
-		sd = socket(AF_INET, SOCK_DGRAM, 0);
-		if (sd == INVALID_SOCKET)
-		{
-			fprintf(stderr, "Could not create socket.\n");
-			WSACleanup();
+			fprintf(stderr, "WSAStartup() failed");
 			return -1;
 		}
 
-		/* Clear out server struct */
-		memset((void *)&sockaddr, '\0', sizeof(struct sockaddr_in));
-
-		/* Set family and port */
-		sockaddr.sin_family = AF_INET;
-		sockadr.sin_port = htons(port_number);
-		sockaddr.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)addr[0];
-		sockaddr.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)addr[1];
-		sockaddr.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)addr[2];
-		sockaddr.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)addr[3];
-		if (bind(sd, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) == -1)
+		/* create a socket for sending to the multicast address */
+		if ((m_sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
 		{
-			fprintf(stderr, "Could not bind name to socket.\n");
-			closesocket(sd);
-			WSACleanup();
+			fprintf(stderr, "socket() failed");
 			return -1;
 		}
 
-		/* Print out server information */
-		printf("Server running on %u.%u.%u.%u\n", (unsigned char)server.sin_addr.S_un.S_un_b.s_b1,
-			   (unsigned char)server.sin_addr.S_un.S_un_b.s_b2,
-			   (unsigned char)server.sin_addr.S_un.S_un_b.s_b3,
-			   (unsigned char)server.sin_addr.S_un.S_un_b.s_b4);
+		/* set the TTL (time to live/hop count) for the send */
+		if ((setsockopt(m_sock, IPPROTO_IP, IP_MULTICAST_TTL,
+						(void *)&mc_ttl, sizeof(mc_ttl))) < 0)
+		{
+			fprintf(stderr, "socket() failed");
+			return -1;
+		}
+
+		/* construct a multicast address structure */
+		memset(&m_sockAddr, 0, sizeof(m_sockAddr));
+		m_sockAddr.sin_family = AF_INET;
+		m_sockAddr.sin_addr.s_addr = inet_addr(m_addrStr);
+		m_sockAddr.sin_port = htons(m_port);
+	}
+
+	void close()
+	{
+		closesocket(m_sock);
+		WSACleanup(); /* Cleanup Winsock */
+	}
+
+	int sendData(const char *buf, int len)
+	{
+				return sendto(m_sock, buf, len, 0,
+						(struct sockaddr *)&m_sockAddr,
+						sizeof(m_sockAddr))) != len);
 	}
 
   private:
-	WSADATA w;							/* Used to open windows connection */
-	unsigned short port_number = 60000; /* Port number to use */
-	int addr[4] = {239, 255, 4, 12};
-	int client_length;			 /* Length of client struct */
-	int bytes_received;			 /* Bytes received from client */
-	SOCKET sd;					 /* Socket descriptor of server */
-	struct sockaddr_in sockaddr; /* Information about the server */
-	char buffer[BUFFER_SIZE];	/* Where to store received data */
-	time_t current_time;		 /* Current time */
+	WSADATA m_wsaData;
+	SOCKET m_sock;
+	char *m_addrStr;
+	unsigned short m_port;
+	struct sockaddr_in m_sockAddr; /* Information about the server */
 }
